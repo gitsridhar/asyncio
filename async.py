113a63935082a7211cdd09352590c79df9129cab
Sridhar Venkat <sridharvenkat@Sridhars-MBP.attlocal.net>
2022-10-11 21:07:56 -0400

    Split count into anothercount.
    Signed-off-by: Sridhar Venkat <sridharvenkat@Sridhars-MBP.attlocal.net>

--- async.py
+++ async.py
@@ -1,15 +1,22 @@
 import asyncio
+import time
 
 async def count():
     print("Before")
     await asyncio.sleep(1)
+
     print("After")
 
+async def anothercount():
+    print("Another Before")
+    await asyncio.sleep(1)
+    print("Another After")
+
 async def main():
-    await asyncio.gather(count(), count(), count())
+    # await asyncio.gather(count(), count(), count())
+    await asyncio.gather(count(), anothercount())
 
 if __name__ == "__main__":
-    import time
     s = time.perf_counter()
     asyncio.run(main())
     elapsed = time.perf_counter() - s
